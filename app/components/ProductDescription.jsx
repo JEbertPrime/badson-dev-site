2c4e581a8c22d9840fb008a2e7019c27436a893e
JEbertPrime <uhsalad@gmail.com>
2025-02-27 19:52:30 -0500

    in toggle

--- app/components/ProductDescription.jsx
+++ app/components/ProductDescription.jsx
@@ -1,12 +1,66 @@
 import {useEffect, useState, useRef} from 'react';
+function isNumeric(str) {
+  if (typeof str != 'string') return false; // we only process strings!
+  return (
+    !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+    !isNaN(parseFloat(str))
+  ); // ...and ensure strings of whitespace fail
+}
+
 export const ProductDescription = ({description}) => {
   const [activeTab, setActiveTab] = useState(0);
   const [minHeight, setMinHeight] = useState(0);
+  const [isInches, setUnit] = useState(true);
+  let [cmValues, setCmValues] = useState([]);
+  let [inchesValues, setInchesValues] = useState([]);
   const descriptionDiv = useRef(null);
   useEffect(() => {
-    if (minHeight < descriptionDiv.current.clientHeight)
+    if (activeTab == 1) {
+      let unit = document.querySelector('table tr:first-child td:first-child');
+      unit.style.cursor = 'pointer';
+      unit.innerHTML = isInches
+        ? "<span class='underline font-bold'><span class='text-green-600'>IN</span> / CM</span>"
+        : "<span class='underline font-bold'>IN / <span class='text-green-600'>CM</span></span>";
+    }
+  }, [activeTab, isInches]);
+  useEffect(() => {
+    if (minHeight < descriptionDiv.current.clientHeight) {
       setMinHeight(descriptionDiv.current.clientHeight);
-  }, [activeTab]);
+    }
+    if (activeTab == 1) {
+      let unit = document.querySelector('table tr:first-child td:first-child');
+      unit.style.cursor = 'pointer';
+      let measurements = Array.from(
+        document.querySelectorAll('table td'),
+      ).filter((td) => isNumeric(td.innerText));
+      const toggleUnit = () => {
+        setUnit((i) => !i);
+      };
+      let inValues, cmVals;
+      if (!inchesValues.length) {
+        inValues = measurements.map((td) => td.innerText);
+        setInchesValues(inValues);
+        cmVals = measurements.map((td) =>
+          Math.trunc(Number(td.innerText) * 2.54),
+        );
+        setCmValues(cmVals);
+      } else {
+        inValues = inchesValues;
+        cmVals = cmValues;
+      }
+      const unitListener = (e) => {
+        toggleUnit();
+        unit.innerHTML = isInches
+          ? "<span class='underline font-bold'><span class='text-green-600'>IN</span> / CM</span>"
+          : "<span class='underline font-bold'>IN / <span class='text-green-600'>CM</span></span>";
+        measurements.forEach(
+          (m, i) => (m.innerText = !isInches ? inValues[i] : cmVals[i]),
+        );
+      };
+      unit.addEventListener('click', unitListener);
+      return () => unit.removeEventListener('click', unitListener);
+    }
+  }, [activeTab, isInches, minHeight]);
   return (
     <section aria-label="Product Description" className="product-description ">
       <div className="flex justify-center gap-6 m-auto">
