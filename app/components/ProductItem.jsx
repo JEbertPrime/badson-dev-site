c6b5b3191f500d3e867a7431c657cc55b0852d70
JEbertPrime <uhsalad@gmail.com>
2025-05-19 22:02:03 -0400

    trench

--- app/components/ProductItem.jsx
+++ app/components/ProductItem.jsx
@@ -15,7 +15,7 @@ export const ProductItem = ({product, loading, className}) => {
   return (
     <Link
       className={
-        'product-item hover:no-underline [#healer-tee&]:scale-110 flex flex-col last-of-type:w-3/5 last-of-type:m-auto h-full group  ' +
+        'product-item hover:no-underline [#healer-tee&]:scale-110 flex flex-col [#paw-jacket-copy&]:w-3/5 [#paw-jacket-copy&]:m-auto h-full group  ' +
         className
       }
       id={product.handle}
